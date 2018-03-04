bdfac10e795df92f00169f3f849bd1aa6333c155
daddolo <davide20799@gmail.com>
2018-03-04 21:43:23 +0100

    Unregistration function

--- index.php
+++ index.php
@@ -5,3 +5,4 @@ $us = new User();
 $us->ConnectDB("localhost", "phplogin", "root", "root");
 $us->Register("davide", "davide@calza.it", "password1234");
 $us->Login("davide", "davide@calza.it", "password1234");
+$us->Unregister("davide", "davide@calza.it", "password1234");
--- user.php
+++ user.php
@@ -31,13 +31,25 @@ class User
     {
         try{
             $q = $this->conn->prepare($query);
-            echo "\n" . $query;
+            //echo "\n" . $query;
             return $q;
         }
         catch (PDOException $e){
             echo "\nException on Query execution: " . $e->getMessage();
         }
         return null;
+    }
+
+    //LoginCheck
+    //  Check whether the user is logged in or not
+    public function LoginCheck()
+    {
+        //Check if session variable usrSession is set and not null
+        if(isset($_SESSION['usrSession']))
+        {
+            return true;
+        }
+        return false;
     }
 
     //Register
@@ -83,7 +95,7 @@ class User
             {
                 if(password_verify($password, $row['password']))
                 {
-                    $_SESSION['user_session'] = $row['id'];
+                    $_SESSION['usrSession'] = $row['id'];
                     echo "\nLogin successful";
                     return true;
                 }
@@ -100,6 +112,46 @@ class User
         catch(Exception $e)
         {
             echo "\nException on login: " . $e->getMessage();
+        }
+        return false;
+    }
+
+    //Unregister
+    //  Unregister a user
+    //  $username = username of the user
+    //  $email    = email of the user
+    //  $password = password of the user
+    public function Unregister($username,$email,$password)
+    {
+        try
+        {
+            $q = $this->Query("SELECT id, username, email, password FROM user WHERE username=:username OR email=:email ");
+            $q->execute(array(':username'=>$username, ':email'=>$email));
+            $row=$q->fetch(PDO::FETCH_ASSOC);
+            if($q->rowCount() == 1)
+            {
+                if(password_verify($password, $row['password']))
+                {
+                    $q = $this->Query("DELETE FROM user WHERE username=:username OR email=:email ");
+                    $q->bindparam(":username", $username);
+                    $q->bindparam(":email", $email);
+                    $q->execute();
+                    echo "\nUnregistration successful";
+                    return true;
+                }
+                else
+                {
+                    throw new Exception("Unregistration failed");
+                }
+            }
+        }
+        catch(PDOException $e)
+        {
+            echo "\nException on unregistration: " . $e->getMessage();
+        }
+        catch(Exception $e)
+        {
+            echo "\nException on unregistration: " . $e->getMessage();
         }
         return false;
     }
