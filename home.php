ed642472b74ef7807e73734cebcfa4f9584cd9e6
daddolo <davide20799@gmail.com>
2018-03-15 12:29:12 +0100

    PHP list of registered users

--- home.php
+++ home.php
@@ -63,34 +63,32 @@ $row = $query->fetch(PDO::FETCH_ASSOC);
     <!--Users list-->
     <div class="col-md-5">
         <div class="list-group" id="div-users-list">
-            <a href="#" class="list-group-item list-group-item-action flex-column align-items-start list-group-item-info active">
-                <div class="row">
-                    <div class="col-md-9">
-                        <div class="d-flex w-100 justify-content-between">
-                            <h5 class="mb-1">username</h5>
-                            <small>joined on: date</small>
+            <?php
+            $users = $user->Query('SELECT username, email, joining FROM user ORDER BY joining DESC');
+            $users->execute();
+            foreach ($users as $r){
+                print('<a href="#" class="list-group-item list-group-item-action flex-column align-items-start">
+                    <div class="row">
+                        <div class="col-md-9">
+                            <div class="d-flex w-100 justify-content-between">
+                                <h5 class="mb-1">' .
+                                    $r['username'] .
+                                '</h5>
+                                <small>joined on:' .
+                                    $r['joining'] .
+                                '</small>
+                            </div>
+                            <p class="mb-1">' .
+                                $r['email'] .
+                            '</p>
                         </div>
-                        <p class="mb-1">email</p>
-                    </div>
-                    <div class="col-md-3">
-                        <button class="btn btn-outline-light btn-lg my-2 my-sm-0 mr-sm-2" type="submit" style="height: 100%; width: 100%;">Modify</button>
-                    </div>
-                </div>
-            </a>
-            <a href="#" class="list-group-item list-group-item-action flex-column align-items-start">
-                <div class="row">
-                    <div class="col-md-9">
-                        <div class="d-flex w-100 justify-content-between">
-                            <h5 class="mb-1">username</h5>
-                            <small>joined on: date</small>
+                        <div class="col-md-3">
+                            <button class="btn btn-outline-info btn-lg my-2 my-sm-0 mr-sm-2" type="submit" style="height: 100%; width: 100%;">Modify</button>
                         </div>
-                        <p class="mb-1">email</p>
                     </div>
-                    <div class="col-md-3">
-                        <button class="btn btn-outline-light btn-lg my-2 my-sm-0 mr-sm-2" type="submit" style="height: 100%; width: 100%;">Modify</button>
-                    </div>
-                </div>
-            </a>
+                </a>');
+            }
+            ?>
         </div>
     </div>
     <div class="col-md-7">
